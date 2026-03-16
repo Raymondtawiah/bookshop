89094b56ba653977ad8c45ba2400b66b246ec17b
Raymond Tawiah <raymondtawiah@example.com>
2026-03-16 21:05:12 +0000

    modified

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,6 @@
         <title>Dashboard - {{ config('app.name', 'Bookshop') }}</title>
         <script src="https://cdn.tailwindcss.com"></script>
         <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="apple-touch-icon" href="/icon.jpg">
         <link rel="manifest" href="/manifest.json">
         <meta name="theme-color" content="#4f46e5" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
