a886b289206473456f680c7f06046a0ecf36bda6
Raymond Tawiah <raymondtawiah@example.com>
2026-03-16 16:13:32 +0000

    modified

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,10 +2,8 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\FortifyServiceProvider;
-use Laravel\Socialite\SocialiteServiceProvider;
 
 return [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
-    SocialiteServiceProvider::class,
 ];
