f337cd644651fcd27b5be9ebd02dece159f2a19e
Raymond Tawiah <raymondtawiah@example.com>
2026-03-17 01:26:39 +0000

    modied

--- config/fortify.php
+++ config/fortify.php
@@ -146,7 +146,7 @@ return [
     'features' => [
         Features::registration(),
         Features::resetPasswords(),
-        Features::emailVerification(),
+        // Features::emailVerification(), // Disabled - users can login without email verification
         Features::twoFactorAuthentication([
             'confirm' => true,
             'confirmPassword' => true,
