7243caf56ae35f0b717eee48adb58d12aa1466fb
Raymond Tawiah <raymondtawiah@example.com>
2026-03-17 19:53:05 +0000

    modified

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
 abstract class Controller
 {
-    //
+    /**
+     * Log the user out of the application.
+     */
+    public function logout()
+    {
+        Auth::guard('web')->logout();
+        Session::invalidate();
+        Session::regenerateToken();
+        
+        return redirect('/');
+    }
 }
