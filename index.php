9757e3104f19678477315a4dbd8cb5d40611f570
Raymond Tawiah <raymondtawiah@example.com>
2026-03-17 16:11:15 +0000

    modified

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
@@ -14,7 +13,8 @@ if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
 require __DIR__.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-/** @var Application $app */
 $app = require_once __DIR__.'/bootstrap/app.php';
+
+$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
 $app->handle(Request::capture());
