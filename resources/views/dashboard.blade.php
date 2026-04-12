af83bad734fdbe3a67c8a242ee79e02116e3e9bb
Raymond Tawiah <raymondtawiah23@gmail.com>
2026-04-12 07:20:47 +0100

    Fix dashboard horizontal scroll

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,7 +23,7 @@
             }
         </style>
     </head>
-    <body class="bg-gray-50 font-sans">
+    <body class="bg-gray-50 font-sans" style="overflow-x: hidden; max-width: 100vw;">
         <x-customer-navbar />
 
       
