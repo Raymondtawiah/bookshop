667528ebd962c871cac47b01880363dd4d8b97b0
Raymond Tawiah <raymondtawiah23@gmail.com>
2026-04-13 12:39:55 +0100

    modified

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -32,7 +32,7 @@ class Book extends Model
             return null;
         }
 
-        return asset('books/' . $this->cover_image);
+        return asset('public/books/' . $this->cover_image);
     }
 
     public function getBookPdfUrlAttribute()
@@ -41,7 +41,7 @@ class Book extends Model
             return null;
         }
 
-        return asset('books/' . $this->book_pdf);
+        return asset('public/books/' . $this->book_pdf);
     }
 
     public function hasCoverImage(): bool
