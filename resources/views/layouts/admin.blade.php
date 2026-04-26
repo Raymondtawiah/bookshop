cfeb41ecb4fe58b1dfb7ac65ef6af1a3b84b03ce
Raymond Tawiah <raymondtawiah23@gmail.com>
2026-04-26 18:02:29 +0100

    modify

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title', 'Admin') - {{ config('app.name', 'Bookshop') }}</title>
-        <script src="https://cdn.tailwindcss.com"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <link rel="icon" href="/favicon.ico" sizes="any">
     </head>
