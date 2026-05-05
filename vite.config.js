fedf64df5d0c92b72a8f53ce42be0bb83a7dca20
Raymond Tawiah <raymondtawiah23@gmail.com>
2026-05-05 13:55:07 +0100

    modfied

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -17,5 +18,9 @@ export default defineConfig({
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
+    },
+    build: {
+        outDir: 'public/build',
+        manifest: true,
     },
 });
